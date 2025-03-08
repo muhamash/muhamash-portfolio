c27cc3c4e571b9728323ad74b349053f7c10e744
muhamsh <muhammad-ashraful@outlook.com>
2025-03-09 01:16:39 +0600

    project detail page init

--- src/app/projects/[id]/page.js
+++ src/app/projects/[id]/page.js
@@ -19,8 +19,9 @@ export default async function ProjectDetailsPage({ params, searchParams }) {
   }
 
   return (
-    <div className="bg-gray-200 text-black">
-      {getParams?.id}
+    <div className="bg-gray-200 text-black text-xl h-screen w-screen flex items-center justify-center">
+      <p>project index : {getParams?.id}</p>
+      <p className="font-code text-red-600 text-4xl">Under construction site!!</p>
     </div>
   );
 }
