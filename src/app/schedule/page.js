8d5e8d34fdb1896336c420a38411b629ff67453c
muhamsh <muhammad-ashraful@outlook.com>
2025-03-16 10:22:59 +0600

    feat(page): schedule page init

---
+++ src/app/schedule/page.js
@@ -0,0 +1,11 @@
+import Modal from "@/components/common/Modal";
+
+export default async function SchedulePage() {
+    return (
+        <Modal>
+            <div className="bg-white text-black p-10">
+                <p>schedule!!</p>
+            </div>
+        </Modal>
+    );
+}
